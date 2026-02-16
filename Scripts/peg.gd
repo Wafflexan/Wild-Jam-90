9cb4ef1094c2405598d8f1321d9740e1c150037e
Wafflexan <101963408+Wafflexan@users.noreply.github.com>
2026-02-15 17:09:14 -0800

    idk bruh

--- Scripts/peg.gd
+++ Scripts/peg.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var attatched : bool = false
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.has_method("launch"):
@@ -11,4 +12,20 @@ func _on_body_entered(body: Node2D) -> void:
 		print(body.linear_velocity.y)
 		if body.linear_velocity.y > -300:
 			print("attatch")
+			if not attatched: attatched = true
 			
+
+
+func _process(delta: float) -> void:
+	if attatched:
+		pass
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.has_method("launch"):
+		print(body.linear_velocity.y)
+	if body.linear_velocity.y > -300:
+		print("attatch")
+		if attatched: attatched = false
+		
+	pass # Replace with function body.
