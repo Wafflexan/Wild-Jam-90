4d950271a16f01e9f4640a66506c0dc410c82d92
Wafflexan <101963408+Wafflexan@users.noreply.github.com>
2026-02-21 18:18:24 -0800

    Revert "Improved start animation"
    This reverts commit c088e5164480117d58df96d140ee54d4824bd947.

--- Scripts/death_scene.gd
+++ Scripts/death_scene.gd
@@ -1,22 +1,18 @@
 extends CanvasModulate
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var button: Button = $Control/Button
-@onready var bus_stop: Sprite2D = $"Bus stop"
-@onready var animation_player_3: AnimationPlayer = $AnimationPlayer3
 
 var pressed : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pressed = false
-	animation_player.play("Bus stop")
-	animation_player_3.play("Death")
-	await animation_player.animation_finished
-	animation_player.play("light beam")
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	await animation_player_3.animation_finished
+	await animation_player.animation_finished
 	SceneManager._change_scene("MAIN")
 
 func _on_button_pressed() -> void:
